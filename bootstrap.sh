c8256de4d99299ded77c54a6fe92f6a6a1e52078
symball <sball@hk.palo-it.com>
2022-05-16 13:21:15 +0800

    Make SDK management a bit more controllable
    * Add boolean flag to determine whether or not environments (e.g. Python, Java, Ruby) should be managed by the script
    * Segregate installation of development kits

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,17 @@
 
 GIT_REPO_PREFIX=git@github.com:palo-it-hk/
 # Use simplified names. "tech-excellence-flutter" "hubspot-palo-it-web"
-PROJECTS_TO_CHECKOUT=()
+PROJECTS_TO_CHECKOUT=("learning-web-1-basic-html-css" "macbook-bootstrap")
 WORKSPACE_PATH="$HOME/palo-it"
 # See sdk list java for options
+JAVA_MANAGED=FALSE
 JAVA_VERSION=17.0.3-tem
 # lowercase LTS name only 
-NODE_LTS_NAME=gallium
+NODE_LTS_NAME=fermium
 # Exact version string
 PYTHON_VERSION=3.9.12
+RUBY_VERSION=3.1.2
+RUBY_MANAGED=FALSE
 
 # Check whether an application is available
 exists()
@@ -105,14 +108,6 @@ export PATH="/usr/local/sbin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
-# Python management
-if command -v pyenv 1>/dev/null 2>&1; then
- eval "$(pyenv init -)"
-fi
-# Ruby Environment Manager
-if command -v rbenv 1>/dev/null 2>&1; then
-    eval "$(rbenv init - zsh)"
-fi
 #
 # END PALOIT
 #
@@ -138,48 +133,40 @@ echo ''
 echo 'Setting up services from brew'
 brew install \
     jq \
-    ktlint \
     nginx \
     nvm \
-    postgresql \
-    pyenv \
-    rbenv
+    postgresql
 
 echo ''
 echo 'Setting up graphical applications from brew'
 brew install --cask \
   dbeaver-community \
   docker \
-  gpg-suite \
   insomnia \
   keepassxc \
   miro \
   openvpn-connect \
-  postman \
-  slack \
   shottr \
   stoplight-studio \
-  tunnelblick \
   visual-studio-code
 
-echo ''
-echo 'Java setup ($JAVA_VERSION) through sdkman'
-curl -s "https://get.sdkman.io" | bash
-sed -i '' \
-  's/sdkman_auto_env=false/sdkman_auto_env=true/g' \
-  ~/.sdkman/etc/config && \
-sed -i '' \
-  's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' \
-  ~/.sdkman/etc/config
+if [ JAVA_MANAGED == TRUE ]
+then
+  echo ''
+  echo 'Java setup ($JAVA_VERSION) through sdkman'
+  curl -s "https://get.sdkman.io" | bash
+  sed -i '' \
+    's/sdkman_auto_env=false/sdkman_auto_env=true/g' \
+    ~/.sdkman/etc/config && \
+  sed -i '' \
+    's/sdkman_auto_answer=false/sdkman_auto_answer=true/g' \
+    ~/.sdkman/etc/config
 
-sdk install java $JAVA_VERSION && \
-sdk install gradle && \
-java -version
+  sdk install java $JAVA_VERSION && \
+  sdk install gradle && \
+  java -version
 
-echo ''
-echo "Python setup (${PYTHON_VERSION}) through pyenv."
-pyenv install $PYTHON_VERSION
-pyenv global $PYTHON_VERSION
+fi
 
 if exists npm
 then
@@ -188,6 +175,51 @@ else
     echo "Installing Node $NODE_LTS_NAME"
     nvm install --lts=$NODE_LTS_NAME
     nvm alias default lts/$NODE_LTS_NAME
+fi
+
+if [ PYTHON_MANAGED == TRUE ]
+then
+  echo ''
+  echo "Python setup (${PYTHON_VERSION}) through pyenv."
+  brew install pyenv
+  if grep -Fxq "# START PYTHON" ~/.zshrc
+    then
+        echo 'Rules already set'
+    else
+        echo 'Rules not yet set'
+        cat >>~/.zshrc << END
+# START PYTHON
+if command -v pyenv 1>/dev/null 2>&1; then
+ eval "$(pyenv init -)"
+fi
+# END PYTHON
+END
+  fi
+  source ~/.zshrc
+  pyenv install $PYTHON_VERSION
+  pyenv global $PYTHON_VERSION
+fi
+
+if [ RUBY_MANAGED == TRUE ]
+then
+  echo ''
+  echo "Ruby setup (${RUBY_VERSION}) through rbenv."
+  brew install rbenv
+  if grep -Fxq "# START RUBY" ~/.zshrc
+    then
+        echo 'Rules already set'
+    else
+        echo 'Rules not yet set'
+        cat >>~/.zshrc << END
+# START RUBY
+if command -v rbenv 1>/dev/null 2>&1; then
+    eval "$(rbenv init - zsh)"
+fi
+END
+  fi
+  source ~/.zshrc
+  rbenv install $RUBY_VERSION
+  rbenv global $RUBY_VERSION
 fi
 
 echo ''
